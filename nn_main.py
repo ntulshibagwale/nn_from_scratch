26e753392255923b1ad362bb5169e1baeba34d41
ntulshibagwale <95308102+ntulshibagwale@users.noreply.github.com>
2021-12-28 14:10:37 -0800

    Update nn_main.py

--- nn_main.py
+++ nn_main.py
@@ -8,26 +8,6 @@ nn_main
 3 layer neural network coded 'from scratch' using Andrew Ng course, translated
 from MATLAB. Used to recognized hand written digits. Eventually will be used
 for spectrogram testing.
-
-Input Layer: 400 input unit for 20x20 unrolled image + 1 bias unit
-Hidden Layer: 25 hidden units + 1 bias unit
-Output Layer: 10 output units used for classification [0->9]
-
-Data:
-    x.csv -> shape[5000x400); 5000 examples of 20x20 pixel handwritten digits
-    y.csv -> shape[5000x1]; class label for each example
-    theta_1.csv -> shape[25x401] -> optim weights from input to hidden layer
-    theta_2.csv -> shape[10x26] -> optim weights from hidden layer to output
-
-The program will generate random initial weight matrices and optimize their
-values by minimizing the neural network cost function. The optimizer uses
-weight gradients computed using backpropagation. The optimizing algorithm is
-from scipy optimization library called Newton Conjugate Gradient, and it 
-runs for 200 iterations.
-
-Data is shuffled and split 80% into training and 20% into test data.
-The program will output the model accuracy on the training data and the 
-visualization of the activation of each the hidden layer units. 
  
 """
 
